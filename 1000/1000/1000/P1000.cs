d8c037042d6e2a12702adfbae6cac35598bac1bc
rhange <rhange0511@gmail.com>
2022-08-31 12:56:20 +0900

    first commit

---
+++ 1000/1000/1000/P1000.cs
@@ -0,0 +1,10 @@
+﻿namespace _1000
+{
+    public static class P1000
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ 1000/1000/1000/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(P1000.add(int.Parse(s[0]), int.Parse(s[1])));
+        }
+    }
+}
